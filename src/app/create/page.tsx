bf36467baaddb550e6fc0df90a637558214f2abf
boby900 <bobyt2265@gmail.com>
2025-07-25 12:42:01 +0530

    feat: added todos

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,11 +1,12 @@
 import { auth } from "../../../auth";
+import { redirect } from "next/navigation";
 import CreateClient from "./CreateClient";
 
 export default async function CreatePage() {
   const session = await auth();
 
   console.log("session", session)
-  if (!session) return <div>Not authenticated</div>
+  if (!session) redirect('/api/auth/signin')
   return <CreateClient/>;
 }
 
